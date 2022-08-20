49ba379d7aa43fb02c5fb5859cee8bed87f253b7
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2022-08-20 16:26:43 +0600

    Inside of bank Balance withdeo and deposit

--- js/logeIn.js
+++ js/logeIn.js
@@ -13,10 +13,12 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // console.log(mail, password);
 
     if (mail == "mamun@gmail.com" && password == 12345678) {
-        console.log("valid user");
+        // console.log("valid user");
+        window.location.href = 'bank.html'
     }
     else {
-        console.log("Invalid user");
+        // console.log("Invalid user");
+        alert("Password / email vule gesos !!! Tor jonno kono tk nai ja vag ");
     }
 
 
